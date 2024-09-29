f8fdf511a651004577ee2d15219144285ca22250
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-09-29 23:25:39 +0330

    User Service Added

---
+++ src/services/user/controller.go
@@ -0,0 +1,18 @@
+package user
+
+import "github.com/kataras/iris/v12"
+
+func (h *userHandler) getUser(ctx iris.Context) {
+}
+
+func (h *userHandler) listUsers(ctx iris.Context) {
+}
+
+func (h *userHandler) createUser(ctx iris.Context) {
+}
+
+func (h *userHandler) deleteUser(ctx iris.Context) {
+}
+
+func (h *userHandler) updateUser(ctx iris.Context) {
+}
