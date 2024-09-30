4a2d9b02f358f63716ae1ead1e2a4646f5288e5b
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-09-30 14:07:08 +0330

    List Users Handler Added

--- src/services/user/controller.go
+++ src/services/user/controller.go
@@ -1,11 +1,33 @@
 package user
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+
+	"go-user-crud-iris-cassandra/src/types"
+)
 
 func (h *userHandler) getUser(ctx iris.Context) {
 }
 
 func (h *userHandler) listUsers(ctx iris.Context) {
+	var users []types.User
+	iter := h.store.db_session.Query("SELECT * FROM users").Iter()
+	var user types.User
+
+	for iter.Scan(&user.ID, &user.Name, &user.Email, &user.Phone) {
+		users = append(users, user)
+	}
+
+	// Error Handling
+	err := iter.Close()
+	if err != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.JSON(iris.Map{"error": "Failed to fetch users"})
+		return
+	}
+
+	// JSON
+	ctx.JSON(users)
 }
 
 func (h *userHandler) createUser(ctx iris.Context) {
