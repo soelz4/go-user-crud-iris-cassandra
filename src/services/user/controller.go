7d978290312315f560d9a9f527df6a44ae9576fc
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-09-30 14:20:10 +0330

    Get User By ID Handler Added

--- src/services/user/controller.go
+++ src/services/user/controller.go
@@ -1,14 +1,37 @@
 package user
 
 import (
+	"github.com/gocql/gocql"
 	"github.com/kataras/iris/v12"
 
 	"go-user-crud-iris-cassandra/src/types"
 )
 
+// GetUser handles GET /users/{id}
 func (h *userHandler) getUser(ctx iris.Context) {
+	id := ctx.Params().Get("id")
+	userID, err := gocql.ParseUUID(id)
+	// Error Handling
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.JSON(iris.Map{"error": "Invalid user ID"})
+		return
+	}
+
+	var user types.User
+	err = h.store.db_session.Query("SELECT * FROM users WHERE id = ? LIMIT 1", userID).
+		Scan(&user.ID, &user.Name, &user.Email, &user.Phone)
+	if err != nil {
+		ctx.StatusCode(iris.StatusNotFound)
+		ctx.JSON(iris.Map{"error": "User not found"})
+		return
+	}
+
+	// JSON
+	ctx.JSON(user)
 }
 
+// ListUsers handles GET /users
 func (h *userHandler) listUsers(ctx iris.Context) {
 	var users []types.User
 	iter := h.store.db_session.Query("SELECT * FROM users").Iter()
